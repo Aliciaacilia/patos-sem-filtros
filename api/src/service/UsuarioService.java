1497d3c9db7706166d04dfc2511c780b5283408c
Alicia Mendes do Nascimento <aliciamnascimento@gmail.com>
2026-01-23 13:07:40 -0300

    Adiciona cadastro de usuario

---
+++ api/src/service/UsuarioService.java
@@ -0,0 +1,22 @@
+package service;
+
+import model.Usuario;
+import repository.UsuarioRepository;
+
+public class UsuarioService {
+
+    private UsuarioRepository repository = new UsuarioRepository();
+
+    public boolean cadastrar(Usuario usuario) {
+
+        if (repository.buscarPorEmail(usuario.getEmail()) != null) {
+            return false;
+        }
+
+        String senhaHash = String.valueOf(usuario.getSenha().hashCode());
+        usuario.setSenha(senhaHash);
+
+        repository.salvar(usuario);
+        return true;
+    }
+}
