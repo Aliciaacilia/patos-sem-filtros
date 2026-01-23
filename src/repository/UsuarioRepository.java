bfd1cc4317c7d472b74e6d8dab5df0e00ed33b71
Alicia Mendes do Nascimento <aliciamnascimento@gmail.com>
2026-01-23 18:15:36 -0300

    Adiciona classes controller

---
+++ src/repository/UsuarioRepository.java
@@ -0,0 +1,26 @@
+package repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import model.Usuario;
+
+public class UsuarioRepository {
+
+    private static List<Usuario> usuarios = new ArrayList<>();
+    private static int proximoId = 1;
+
+    public void salvar(Usuario usuario) {
+        usuario.setId(proximoId++);
+        usuarios.add(usuario);
+    }
+
+    public Usuario buscarPorEmail(String email) {
+        for (Usuario u : usuarios) {
+            if (u.getEmail().equalsIgnoreCase(email)) {
+                return u;
+            }
+        }
+        return null;
+    }
+}
+
