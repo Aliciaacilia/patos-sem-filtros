a5a11d9c278ea325a5756ca6e26c14b1e913d26d
Alicia Mendes do Nascimento <aliciamnascimento@gmail.com>
2026-01-23 12:13:16 -0300

    Adiciona classe usuarioModerador

---
+++ api/src/UsuarioModerador.java
@@ -0,0 +1,22 @@
+public class UsuarioModerador extends Usuario {
+
+    private String nivelAcesso;
+
+    public UsuarioModerador() {
+        super();
+    }
+
+    public UsuarioModerador(int id, String nome, String email, String senha,
+                             boolean emailVerificado, String tipo, String nivelAcesso) {
+        super(id, nome, email, senha, emailVerificado, tipo);
+        this.nivelAcesso = nivelAcesso;
+    }
+
+    public String getNivelAcesso() {
+        return nivelAcesso;
+    }
+
+    public void setNivelAcesso(String nivelAcesso) {
+        this.nivelAcesso = nivelAcesso;
+    }
+}
