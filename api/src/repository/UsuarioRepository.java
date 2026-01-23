838044fa4813a363e7e629c2131e103df69d1af8
Alicia Mendes do Nascimento <aliciamnascimento@gmail.com>
2026-01-23 13:05:17 -0300

    Adiciona repository de usuario

--- api/src/repository/UsuarioRepository.java
+++ api/src/repository/UsuarioRepository.java
@@ -1,2 +1,25 @@
-public void salvar(Usuario usuario)
-public Usuario buscarPorEmail(String email)
+package repository;
+
+import model.Usuario;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UsuarioRepository {
+
+    private static List<Usuario> usuarios = new ArrayList<>();
+    private static int proximoId = 1;
+
+    public void salvar(Usuario usuario) {
+        usuario.setId(proximoId++);
+        usuarios.add(usuario);
+    }
+
+    public Usuario buscarPorEmail(String email) {
+        for (Usuario u : usuarios) {
+            if (u.getEmail().equalsIgnoreCase(email)) {
+                return u;
+            }
+        }
+        return null;
+    }
+}
