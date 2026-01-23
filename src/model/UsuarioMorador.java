73b26729e497ddd9da218e9eaac0358d2679ce07
Alicia Mendes do Nascimento <aliciamnascimento@gmail.com>
2026-01-23 18:04:49 -0300

    Adicionando classes subclasses de Usuario

---
+++ src/model/UsuarioMorador.java
@@ -0,0 +1,32 @@
+package model;
+
+public class UsuarioMorador extends Usuario {
+
+    private String cpf;
+
+    public UsuarioMorador() {
+        super();
+    }
+
+    public UsuarioMorador(int id, String nome, String email, String senha,
+                           boolean emailVerificado, String tipo, String cpf) {
+        super(id, nome, email, senha, emailVerificado, tipo);
+        this.cpf = cpf;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
