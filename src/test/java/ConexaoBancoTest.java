3263a4c409277b897b1ef2698489adaf7dbf9e6e
Alicia Mendes do Nascimento <aliciamnascimento@gmail.com>
2026-03-14 09:57:54 -0300

    Adiciona teste de conexão com o banco de dados

---
+++ src/test/java/ConexaoBancoTest.java
@@ -0,0 +1,23 @@
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import org.junit.jupiter.api.Test;
+
+public class ConexaoBancoTest {
+
+    @Test
+    void testarConexaoComPostgreSQL() {
+        String url = "jdbc:postgresql://localhost:5432/patos_sem_filtros?charSet=UTF-8";
+        String usuario = "postgres";
+        String senha = "alicia06";
+
+        try (Connection conexao = DriverManager.getConnection(url, usuario, senha)) {
+            assertNotNull(conexao, "A conexão não deveria ser nula!");
+            System.out.println("Conexão com o banco realizada com sucesso!");
+        } catch (Exception e) {
+            fail("Falha ao conectar ao banco: " + e.getMessage());
+        }
+    }
+}
