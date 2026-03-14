a6f337c497e1d6a7efe8bc93e0fd807f05b4b566
Alicia Mendes do Nascimento <aliciamnascimento@gmail.com>
2026-03-14 16:22:48 -0300

    Organiza teste de login usando BeforeEach

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,36 +1,33 @@
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import model.Usuario;
 
 public class LoginTest {
 
+    private Usuario user;
+
+    @BeforeEach
+    void setup() {
+        String senhaCadastrada = String.valueOf("alicia123".hashCode());
+        user = new Usuario(1, "Alicia", "alicia@email.com", senhaCadastrada, true, "MORADOR");
+    }
+
     @Test
     void testLoginSucesso() {
-        String senhaCadastrada = String.valueOf("alicia123".hashCode());
-        Usuario user = new Usuario(1, "Alicia", "alicia@email.com", senhaCadastrada, true, "MORADOR");
-        
         String tentativaHash = String.valueOf("alicia123".hashCode());
-
         assertEquals(user.getSenha(), tentativaHash);
     }
 
     @Test
     void testLoginSenhaIncorreta() {
-        String senhaCadastrada = String.valueOf("alicia123".hashCode());
-        Usuario user = new Usuario(1, "Alicia", "alicia@email.com", senhaCadastrada, true, "MORADOR");
-        
         String tentativaErradaHash = String.valueOf("senhaErrada".hashCode());
-
         assertNotEquals(user.getSenha(), tentativaErradaHash);
     }
 
     @Test
     void testLoginDadosVazios() {
-        String senhaCadastrada = String.valueOf("alicia123".hashCode());
-        Usuario user = new Usuario(1, "Alicia", "alicia@email.com", senhaCadastrada, true, "MORADOR");
-
         String tentativaVaziaHash = String.valueOf("".hashCode());
-
         assertNotEquals(user.getSenha(), tentativaVaziaHash);
     }
 }
